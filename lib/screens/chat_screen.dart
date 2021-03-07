73eb4c1893f920ed1ddc0b7da24500230e620fd4
Artur Teixeira <arturjnt@gmail.com>
2021-03-07 21:48:27 +0000

    spacings

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -35,6 +36,7 @@ class ChatScreen extends StatelessWidget {
         )
       ]),
       body: Container(
+        margin: EdgeInsets.all(5),
         child: Column(children: [
           Expanded(child: Messages()),
           NewMessage(),
